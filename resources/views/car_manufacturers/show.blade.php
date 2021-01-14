325184797e25361c4f23043c07383dcee504fc76
aranhid <vladmir909@mail.ru>
2021-01-15 06:29:53 +0800

    Fixed typo

--- resources/views/car_manufacturers/show.blade.php
+++ resources/views/car_manufacturers/show.blade.php
@@ -1,7 +1,7 @@
 @extends('database.templates.show')
 
 @section('title')
-    {{ $carmanufacturer->name }}
+    {{ $carManufacturer->name }}
 @endsection
 
 @section('route_index')
@@ -13,19 +13,19 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Name:</strong>
-                {{ $carmanufacturer->name }}
+                {{ $carManufacturer->name }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Date Created:</strong>
-                {{ date_format($carmanufacturer->created_at, 'jS M Y') }}
+                {{ date_format($carManufacturer->created_at, 'jS M Y') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Date Updated:</strong>
-                {{ date_format($carmanufacturer->updated_at, 'jS M Y') }}
+                {{ date_format($carManufacturer->updated_at, 'jS M Y') }}
             </div>
         </div>
     </div>
