de15418b2f53878747ecba1ea7b79280c5537fe2
aranhid <vladmir909@mail.ru>
2021-01-15 05:07:46 +0800

    Added index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Databases</h2>
+    <div>
+        <ul>
+            <li>
+                <a href="{{ route('positions.index') }}">Positions</a>
+            </li>
+            <li>
+                <a href="{{ route('staff.index') }}">Staff</a>
+            </li>
+        </ul>
+    </div>
+@endsection
